f246fb4a286d77e6b50b8339508c8f240b71a9dd
Tyler Conley <tjacobconley1@gmail.com>
2021-10-12 21:52:09 -0400

    DogeBal.py
    insert your own twitter keys and doge address
    YOU MUST ADD YOUR PC's EXTERNAL IP ADDRESS TO API ACCESS LIST AT https://block.io/dashboard/settings

---
+++ DogeBalTweet.py
@@ -0,0 +1,45 @@
+
+
+# TEST NET
+# https://block.io/api/v2/get_balance/?api_key=c562-fd68-d1b9-b1a5
+
+# import urllib library
+from urllib.request import urlopen
+import json
+from twython import Twython
+
+#KEYS FROM THE TWITTER DEV PORTAL
+consumer_key        = 'ABCDEFGHIJKLKMNOPQRSTUVWXYZ'
+consumer_secret     = '1234567890ABCDEFGHIJKLMNOPQRSTUVXYZ'
+access_token        = 'ZYXWVUTSRQPONMLKJIHFEDCBA'
+access_token_secret = '0987654321ZYXWVUTSRQPONMLKJIHFEDCBA'
+
+
+
+def getDogeBal():
+      
+    url = "https://block.io/api/v2/get_balance/?api_key=c562-fd68-d1b9-b1a5"
+    
+    #store the response of URL
+    response = urlopen(url)
+    
+    # storing the JSON response 
+    # from url in data
+    data_json = json.loads(response.read())
+    #print(data_json)
+    return data_json
+
+def tweetDogeBal():
+    twitter = Twython(
+    consumer_key, consumer_secret, access_token, access_token_secret)
+    message = str(getDogeBal())
+    twitter.update_status(status=message)
+    print("Tweeted: %s" % message)
+
+getDogeBal()
+tweetDogeBal()
+
+
+
+
+
